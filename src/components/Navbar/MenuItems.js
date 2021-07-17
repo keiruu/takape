b51d0e09093d346651da9033e28bbc192d2a685c
Abigail Kaye Unating <abigailkaye.unating@wvsu.edu.ph>
2021-07-17 13:48:22 +0800

    Navbar Finished

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,32 @@
+export const MenuItems = [
+    {
+        title: "Home",
+        url: "#",
+        cName: "nav-links"
+    },
+
+    {
+        title: "Cafes",
+        url: "#",
+        cName: "nav-links"
+    },
+
+    {
+        title: "About",
+        url: "#",
+        cName: "nav-links"
+    },
+
+    {
+        title: "Contact Us",
+        url: "#",
+        cName: "nav-links"
+    },
+
+    {
+        title: "Sign Up",
+        url: "#",
+        cName: "nav-links-mobile"
+    },
+
+]
