5c6dac6e04953974275ba1850dc3c54fabd29851
Abigail Kaye Unating <abigailkaye.unating@wvsu.edu.ph>
2021-09-24 20:37:18 +0800

    Tweaked pages

--- frontend/src/components/Carousel.js
+++ frontend/src/components/Carousel.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import Carousel, { slidesToShowPlugin } from '@brainhubeu/react-carousel';
-import '@brainhubeu/react-carousel/lib/style.css';
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+import Carousel, { slidesToShowPlugin } from '@brainhubeu/react-carousel'
+import '@brainhubeu/react-carousel/lib/style.css'
 
 const provinces = [
     { name: 'Capiz', img: 'https://2.bp.blogspot.com/-zXXEUPu8pzc/WN4WCf3i9wI/AAAAAAAAozk/I6M_CoEaiWM0c5MxTYPPmPhkmrNUz_tBwCLcB/s1600/Capiz%2BBridge.jpg'},
@@ -8,34 +9,41 @@ const provinces = [
     { name: 'Aklan', img: 'https://toprealty.com.ph/wp-content/uploads/2017/06/boracay-beach-front-lots-for-sale-malay-aklan-philippines.jpg'},
     { name: 'Antique', img: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/b8/e7/10/view-from-the-top.jpg?w=500&h=300&s=1'},
 ]
+
+
 export default function MyCarousel() { 
 
+    const history = useHistory()
+
     return(
-        <div className="md:w-2/3 w-full">
-            <Carousel 
-            plugins={[
-                'centered',
-                'infinite',
-                'arrows',
-                {
-                    resolve: slidesToShowPlugin,
-                    options: {
-                    numberOfSlides: 2,
-                }
-                },
-            ]}   
-        >
-            {provinces.map((item) => (
-                <div className="relative bg-black rounded-xl overflow-hidden cursor-pointer">
-                    <p className="absolute top-24 left-0 right-0 font-bold text-white text-2xl">{item.name}</p>
-                    <img
-                        src={item.img}
-                        alt=""
-                        className="transition-all transform hover:scale-125 opacity-50 w-96 h-56 rounded-xl"
-                    />
-                </div>
-            ))}
-        </Carousel>
+        <div className="flex flex-col justify-center items-center">
+            <p className="text-lg text-brown my-6">Provinces within Panay Island</p>
+            <div className="lg:w-2/3 w-full flex flex-col lg:flex-row gap-4">
+                {/* <Carousel 
+                plugins={[
+                    'centered',
+                    'infinite',
+                    'arrows',
+                    {
+                        resolve: slidesToShowPlugin,
+                        options: {
+                        numberOfSlides: 2,
+                    }
+                    },
+                ]}   
+            > */}
+                {provinces.map((item) => (
+                    <div onClick={() => history.push('/cafes')} className="relative bg-black rounded-xl overflow-hidden cursor-pointer">
+                        <p className="absolute top-24 left-0 right-0 font-bold text-white text-2xl">{item.name}</p>
+                        <img
+                            src={item.img}
+                            alt=""
+                            className="transition-all transform hover:scale-125 opacity-50 w-96 h-56 rounded-xl"
+                        />
+                    </div>
+                ))}
+            {/* </Carousel> */}
+            </div>
         </div>
     )
     
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -6,6 +6,9 @@ import { CafeContext } from '../contexts/CafeContext'
 import React, {useContext, useRef, useEffect} from 'react'
 import { Link } from 'react-router-dom'
 
+import ScrollAnimation from 'react-animate-on-scroll'
+import Fade from 'react-reveal/Fade'
+
 import Saly from '../img/Saly-2.png'
 import { loadAnimation } from "lottie-web"
 import { defineLordIconElement } from "lord-icon-element"
@@ -26,51 +29,55 @@ export default function Home() {
         scrollRef.current.scrollIntoView() 
    }
 
-    // useEffect(() => {
-    //     cafe.retrieveCafes()
-    // })
-  
-
     return (
-        <div>
+        <div className="md:bg-white">
             <Navbar/>
-            <div className="flex justify-center items-center pt-6 lg:m-0 m-4 lg:my-0 my-10">
-                <div className="grid gap-y-6">
-                    <h1 className="font-bold text-4xl lg:text-5xl text-accent">Looking for a<br/>place to chill?</h1>
-                    <p className="w-72 md:w-96 text-lg lg:text-xl text-brown">Check out all the listed cafes we have from within the <b>Panay Island</b> in the Philippines. </p>
-                    <div>
-                        <Link to="/cafes">
-                                <button className="transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 bg-bggradient text-white p-4 px-14 rounded-xl shadow-light my-4 font-semibold">Find Cafes</button>
-                        </Link>
-                    </div>
+            
+                <div className="flex bg-bgcolor rounded-b-0 md:rounded-b-1/2 justify-center items-center pt-6 lg:m-0 p-4 lg:my-0 py-10">
+                    <Fade left cascade>
+                        <div className="grid gap-y-6">
+                            <h1 className="font-bold text-4xl lg:text-5xl text-accent md:pt-0 pt-10">Looking for a<br/>place to chill?</h1>
+                            <p className="w-72 md:w-96 text-lg lg:text-xl text-brown">Check out all the listed cafes we have from within the <b>Panay Island</b> in the Philippines. </p>
+                            <div >
+                                <Link to="/cafes">
+                                        <button className="transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 bg-bggradient text-white p-4 px-14 rounded-xl shadow-light my-4 font-semibold">Find Cafes</button>
+                                </Link>
+                            </div>
+                        </div>
+                    </Fade>
+                    <Fade right>
+                        <img
+                            className="w-96 lg:w-100 md:block hidden"
+                            src={Saly}
+                            alt=""
+                        />
+                    </Fade>
                 </div>
-                <img
-                    className="w-96 lg:w-100 md:block hidden"
-                    src={Saly}
-                    alt=""
-                />
-            </div>
 
-            <div id='about' ref={aboutRef} className="bg-white text-center py-10 lg:py-24 flex flex-col items-center justify-center gap-4">
-                <p className="mt-10 font-semibold text-xl text-brown">About Takape</p>
-                <p className="text-brown w-64 md:w-medsm lg:text-5xl md:text-4xl text-3xl">Takape is a simple website that aims to help you find cafes within Panay Island.</p>
-                {/* <p>Made by <a href='#'>Abigail</a></p> */}
+            <Fade bottom cascade>
+                <div id='about' ref={aboutRef} className="bg-white text-center py-10 lg:py-24 flex flex-col items-center justify-center gap-4">
+                    <p className="mt-10 font-semibold text-xl text-brown">About Takape ☕</p>
+                    <p className="text-brown w-64 md:w-medsm lg:text-5xl md:text-4xl text-3xl">Takape is a simple website that aims to help you find cafes within Panay Island.</p>
+                    {/* <p>Made by <a href='#'>Abigail</a></p> */}
 
-                <Carousel/>
-            </div>
+                    <Carousel/>
+                </div>
+            </Fade>
 
-            <div id='contactus' ref={contactRef} className="text-brown bg-white text-center py-20 pt-10 flex flex-col items-center">
-                <a href="mailto:abbyunat@gmail.com" className="w-96 bg-bgcolor cursor-pointer hover:opacity-70 transition-all p-6 pt-4 rounded-xl">
-                        <lord-icon
-                            src="https://cdn.lordicon.com/rhvddzym.json"
-                            trigger="loop"
-                            colors="primary:#83564b,secondary:#83564b"
-                            style={{width:50, height:50}}>
-                        </lord-icon>
-                    <p className="text-md lg:text-2xl font-semibold">Get in touch!</p>
-                    <p className="text-md lg:text-2xl">abbyunat@gmail.com</p>
-                </a>
-            </div>
+            <ScrollAnimation animateIn="fadeIn">
+                <div id='contactus' ref={contactRef} className="text-brown bg-white text-center py-20 pt-10 flex flex-col items-center">
+                    <a href="mailto:abbyunat@gmail.com" className="w-96 bg-bgcolor cursor-pointer hover:opacity-70 transition-all p-6 pt-4 rounded-xl">
+                            <lord-icon
+                                src="https://cdn.lordicon.com/rhvddzym.json"
+                                trigger="loop"
+                                colors="primary:#83564b,secondary:#83564b"
+                                style={{width:50, height:50}}>
+                            </lord-icon>
+                        <p className="text-md lg:text-2xl font-semibold">Get in touch!</p>
+                        <p className="text-md lg:text-2xl">abbyunat@gmail.com</p>
+                    </a>
+                </div>
+            </ScrollAnimation>
             <Footer/>
         </div>
     )
